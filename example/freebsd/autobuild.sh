057cfd1bcb33c37763a3a1bbe217be4b297d2555
kei-g <km.8k6ce+github@gmail.com>
2023-01-21 14:35:19 +0900

    :hammer: Add /usr/lib/llvm-$version/bin to PATH in order to make `llvm-ar` appearable
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- example/freebsd/autobuild.sh
+++ example/freebsd/autobuild.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -e
+llvmdir=$(find /usr/lib -maxdepth 1 -name llvm-\* -type d -exec stat -c '%X %n' {} \; \
+	| sort -nr \
+	| awk 'NR==1,NR==3 {print $2}' \
+	| head -n1)
+PATH=$llvmdir/bin:$PATH
 for arch in amd64 arm64 armv6 armv7 i386; do
 	CC_LD=lld \
 	LDFLAGS=-fuse-ld=lld \
